735c864a7ef7f880e04300fbd269481155627383
Vajira Kavindi <68498264+vaji123@users.noreply.github.com>
2021-10-02 08:37:29 +0530

    Update index.php

--- app_code/templates/index.php
+++ app_code/templates/index.php
@@ -1,5 +1,5 @@
-<?php include("new.html")?>
-<?php include("after.html")?>
-<?php include("chart.html")?>
-<?php include("home.html")?>
-<?php include("home1.html")?>
+<?php include("new.html"); ?>
+<?php include("after.html"); ?>
+<?php include("chart.html"); ?>
+<?php include("home.html"); ?>
+<?php include("home1.html"); ?>
